be3ca1e696a18d3a2d7a11869509f32981bfd9ed
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-16 19:10:39 +0500

    1.3.3

---
+++ router.php
@@ -0,0 +1,35 @@
+<?php
+
+// Getting uri and storing it in a variable. (Adding parse_url is removeing or disallowing query's, so we can use reoute like this more secure)
+$uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
+
+
+
+//Making an array to store routes.
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+
+
+// Now we made just one line routing.
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort(404);
+    }
+}
+
+//function gor errors
+function abort($code) {
+    http_response_code(404);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
+routeToController($uri, $routes);
