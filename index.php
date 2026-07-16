a57af78bd985fe621910315a3776849eb498e94f
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-16 11:30:25 +0500

    1.3.1

--- index.php
+++ index.php
@@ -3,13 +3,26 @@
 require "functions.php";
 
 
-// Getting uri and storing it in a variable
-$uri = $_SERVER['REQUEST_URI'];
+// Getting uri and storing it in a variable. (Adding parse_url is removeing or disallowing query's, so we can use reoute like this more secure)
+$uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
 
-if($uri === '/') {
-    require 'controllers/index.php';
-} else if($uri === '/contact') {
-    require 'controllers/contact.php';
-} else if($uri === '/about') {
-    require 'controllers/about.php';
+
+
+//Making an array to store routes.
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+
+// Now we made just one line routing.
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+
+    require 'views/404.php';
+
+    die();
 }
