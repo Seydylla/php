aadc9852358554b319df03e342b6ae0aad8f5ca5
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-17 17:05:00 +0500

    1.4.7

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ require "Database.php";
 $config = require 'config.php';
 $db = new Database($config['database']);
 
+// Securing SQLinjection
+
+
 $id = $_GET['id'];
-$query = "select * from posts where id = ?";
+$query = "select * from posts where id = :id";
 
 
-$posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
