b996c2ad3930319fbdcac004ea68ab90822c5243
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-17 17:03:15 +0500

    1.4.6

--- index.php
+++ index.php
@@ -4,8 +4,12 @@ require "functions.php";
 require "Database.php";
 
 $config = require 'config.php';
-
 $db = new Database($config['database']);
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+
+
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
