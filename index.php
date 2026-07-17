5234cc345661ac71857530e265cef8e37a9e7ac3
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-17 14:06:42 +0500

    1.4.3

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <?php
 
 require "functions.php";
-
-// Connect to the database, and execute a query
-class Database {
-
-    public $connection;
-
-    // Costruct always work
-    public function __construct()
-    {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
-    }
-
-
-    public function query($query) {
-
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement;
-    }
-}
+require "Database.php";
 
 $db = new Database();
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
