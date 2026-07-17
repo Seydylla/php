68644d6c9a38f0020ae0a7c856a31c627f2553fa
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-17 13:56:18 +0500

    1.4.1

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
 require "functions.php";
-// require 'router.php';
 
-//Connect to Mysql database(PDO)
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
+// Connect to the database, and execute a query
+class Database {
+    public function query($query) {
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
+        $pdo = new PDO($dsn);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$db = new Database();
+$posts = $db->query("select * from posts");
+
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
