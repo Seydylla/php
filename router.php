c7770b539a7406072b2529c3a11d24e1993d8122
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-18 23:46:56 +0500

    1.5.1

--- router.php
+++ router.php
@@ -10,6 +10,7 @@ $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
     '/contact' => 'controllers/contact.php',
 ];
 
