e0c275fcefdcb1d12ba288a42f48b2524accd2a4
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-21 22:20:28 +0500

    1.6.0

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,19 @@ $db = new Database($config['database']);
 $heading = 'Create note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 3
-    ]);
+
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 3
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -14,7 +14,10 @@
                         <div class="mt-2">
                             <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" placeholder="Here' an idea for a note"></textarea>
                         </div>
-                            <p class="mt-3 text-sm/6 text-gray-600 dark:text-gray-400">Write a few sentences about yourself.</p>
+
+                        <?php if (isset($errors['body'])) : ?>
+                            <p class="mt-3 text-sm/6 text-red-600 dark:text-red-400"><?= $errors['body'] ?></p>
+                        <?php endif; ?>
                     </div>
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
