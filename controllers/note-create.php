eca9f8bc5f8e7050cb5fabe46513644a4c3228de
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-21 22:27:33 +0500

    1.6.1

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -13,6 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors['body'] = 'A body is required';
     }
 
+    if(strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 charachters';
+    }
+
     if(empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
             'body' => $_POST['body'],
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -12,7 +12,7 @@
                     <div class="col-span-full">
                         <label for="about" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Body</label>
                         <div class="mt-2">
-                            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" placeholder="Here' an idea for a note"></textarea>
+                            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" placeholder="Here' an idea for a note"><?= $_POST['body'] ?? '' ?></textarea>
                         </div>
 
                         <?php if (isset($errors['body'])) : ?>
