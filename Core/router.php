1f09099b57bdc2044e9bf3dde1815674566175ce
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-25 22:30:39 +0500

    1.7.2

--- Core/router.php
+++ Core/router.php
@@ -4,6 +4,7 @@
 // Now we made just one line routing.
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
+        // goes to the file
         require base_path($routes[$uri]);
     } else {
         abort(404);
@@ -20,6 +21,7 @@ function abort($code = 404) {
 }
 
 // Getting uri and storing it in a variable. (Adding parse_url is removeing or disallowing query's, so we can use reoute like this more secure)
+// Getting the file
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
 
