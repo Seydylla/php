b17c61b1dee2692eb646e0df308eda6c3f26ba1e
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-27 23:25:41 +0500

    1.7.6

--- controllers/notes/note.php
+++ controllers/notes/note.php
@@ -13,10 +13,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         'id' => $_GET['id']
     ])->findOrFail();
 
+    authorize($note['user_id'] === $currentUserId);
+
 
     $db->query('delete from notes where id = :id', [
         'id' => $_GET['id']
     ]);
+
+    header('location: /notes');
+    exit();
 } else {
 
     $note = $db->query('select * from notes where id = :id', [
