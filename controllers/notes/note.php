a0c1bdc6c46deb7b2d92b23326f6515de6d718df
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-27 23:23:49 +0500

    1.7.5

--- controllers/notes/note.php
+++ controllers/notes/note.php
@@ -4,11 +4,20 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
+$currentUserId = 3;
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     //form was submittes, delete the note
-} else {
-    $currentUserId = 3;
 
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+} else {
 
     $note = $db->query('select * from notes where id = :id', [
         'id' => $_GET['id']
