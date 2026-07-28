17d0144c74a81a9a45de48ac3d92840dd3e428f9
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-28 22:31:25 +0500

    1.7.7

--- Core/router.php
+++ Core/router.php
@@ -1,28 +1,73 @@
 <?php
 
 
-// Now we made just one line routing.
-function routeToController($uri, $routes) {
-    if (array_key_exists($uri, $routes)) {
-        // goes to the file
-        require base_path($routes[$uri]);
-    } else {
-        abort(404);
+class Router {
+
+    public $routes = [];
+
+    public function get($uri, $controller) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'GET'
+        ];
+    }
+
+    public function post($uri, $controller) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'POST'
+        ];
+    }
+
+    public function delete($uri, $controller) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'Delete'
+        ];
+    }
+
+    public function patch($uri, $controller) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PATCH'
+        ];
+    }
+
+    public function put($uri, $controller) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PUT'
+        ];
     }
 }
 
-//function gor errors
-function abort($code = 404) {
-    http_response_code(404);
+// // Now we made just one line routing.
+// function routeToController($uri, $routes) {
+//     if (array_key_exists($uri, $routes)) {
+//         // goes to the file
+//         require base_path($routes[$uri]);
+//     } else {
+//         abort(404);
+//     }
+// }
 
-    require base_path("views/{$code}.php");
+// //function gor errors
+// function abort($code = 404) {
+//     http_response_code(404);
 
-    die();
-}
+//     require base_path("views/{$code}.php");
 
-// Getting uri and storing it in a variable. (Adding parse_url is removeing or disallowing query's, so we can use reoute like this more secure)
-// Getting the file
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
+//     die();
+// }
 
-routeToController($uri, $routes);
+// // Getting uri and storing it in a variable. (Adding parse_url is removeing or disallowing query's, so we can use reoute like this more secure)
+// // Getting the file
+// $routes = require base_path('routes.php');
+// $uri = parse_url($_SERVER['REQUEST_URI']) ['path'];
+
+// routeToController($uri, $routes);
