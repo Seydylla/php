bbf54dafe14ccd937946e491c67ace0bac6f45a9
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-30 23:40:13 +0500

    1.8.7

--- Core/App.php
+++ Core/App.php
@@ -5,6 +5,10 @@ class App{
     protected static $container;
 
     public static function setContainer($container) {
+        static::$container = $container;
+    }
 
+    public static function container() {
+        return static::$container;
     }
 }
