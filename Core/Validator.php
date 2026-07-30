6da311e8ace2c6aaa966c829dca4a5e794ff06a8
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-30 17:35:19 +0500

    1.8.4

--- Core/Validator.php
+++ Core/Validator.php
@@ -3,7 +3,7 @@
 namespace Core;
 
 class Validator{
-    public function string($value, $min = 1, $max = INF) {
+    public static function string($value, $min = 1, $max = INF) {
         $value = trim($value);
 
         return strlen($value) >= $min && strlen($value) <= $max;
