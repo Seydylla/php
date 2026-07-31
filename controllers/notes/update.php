b4ebf6eec27e3671e0d5425bbc63c610733ab607
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-31 23:34:35 +0500

    1.9.4

--- controllers/notes/update.php
+++ controllers/notes/update.php
@@ -1,3 +1,37 @@
 <?php
 
-dd('updating');
+use Core\App;
+use Core\Validator;
+use Core\Database;
+
+$db = App::container()->resolve(Database::class);
+
+$currentUserId = 3;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$errors = [];
+
+if(! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 charachter is required';
+}
+
+if(count($errors)) {
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('update notes set body = :body where id = :id', [
+    'id' => $_POST['id'],
+    'body' => $_POST['body']
+]);
+
+header('location: /notes');
+die();
--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -5,7 +5,9 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 dark:text-white">
-        <form method="POST" action="/notes">
+        <form method="POST" action="/note">
+            <input type="hidden" name="_method" value="PATCH">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12 dark:border-white/10">
 
@@ -22,7 +24,7 @@
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
                         <a href="/notes" type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-red-500 dark:shadow-none dark:focus-visible:outline-indigo-500">Cancel</a>
-                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:focus-visible:outline-indigo-500">Save</button>
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:focus-visible:outline-indigo-500">Update</button>
                     </div>
 
                 </div>
