6342dc179a76148946ae59ea88b6a9147e87d45a
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-02 23:47:08 +0500

    1.9.12

--- Core/router.php
+++ Core/router.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace Core;
-use Core\Middleware\Auth;
-use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
 
 
 class Router {
@@ -52,15 +51,13 @@ class Router {
 
             //Apply the middleware
 
-            if($route['middleware'] === 'guest') {
-                (new Guest)->handle();
+            if($route['middleware']) {
+                $middleware = Middleware::MAP[$route['middleware']];
+
+                (new $middleware)->handle();
             }
 
-            if($route['middleware'] === 'auth') {
-                (new Auth)->handle();
-            }
-
-                return require base_path($route['controller']);
+            return require base_path($route['controller']);
             }
         }
 
